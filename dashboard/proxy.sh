3a428d382133de22021e8bed0dcff5f8a846985c
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-09-19 13:43:09 +0000

    dashboard/proxy.sh: services listed before starting the proxy

--- dashboard/proxy.sh
+++ dashboard/proxy.sh
@@ -6,6 +6,7 @@
 # Dashboard URL: https://localhost:8443/
 
 set -x
+kubectl -n kubernetes-dashboard get svc -o wide
 kubectl -n kubernetes-dashboard port-forward svc/kubernetes-dashboard-kong-proxy 8443:443
 { set +x; } 2>/dev/null
 
