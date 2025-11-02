06311552a84baf1820bf243a4bdc3d63a0eb05c9
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-11-02 16:06:18 +0000

    talos: cluster created with 2 control planes and 2 workers

--- talos/install.sh
+++ talos/install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-set -e -o pipefail
+[[ "${CONTROLPLANES}" = "" ]] && CONTROLPLANES=2
+[[ "${WORKERS}" = "" ]] && WORKERS=2
 
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | /usr/bin/env NONINTERACTIVE=1 /bin/bash
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew install siderolabs/tap/talosctl kubernetes-cli
-talosctl cluster create
+
+talosctl cluster create \
+  --controlplanes $CONTROLPLANES \
+  --workers $WORKERS
 
 cat > /home/vagrant/.bashrc << __EOF
 eval "\$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
