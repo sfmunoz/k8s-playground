7747fc395041a4cb9d0185d6c55262f3de07124e
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-12-12 17:40:53 +0000

    crd-operator: 'install.sh' executes kopf together with kubectl apply/delete iterations

--- crd-operator/operator/install.sh
+++ crd-operator/operator/install.sh
@@ -8,15 +8,39 @@ set -e -o pipefail
 
 cd "$(dirname "$0")"
 
+if [ ! -d "$DNAME" ]
+then
+  set -x
+  python3 -m virtualenv "$DNAME"
+  "${DNAME}/bin/pip3" install kopf
+  { set +x; } 2> /dev/null
+fi
+
 set -x
-[ -d "$DNAME" ] || python3 -m virtualenv "$DNAME"
-
-"${DNAME}/bin/pip3" install kopf
-
 # peering split into CRD and OBJ to prevent errors
 kubectl apply -f 01-peering-crd.yaml
 kubectl apply -f 02-peering-obj.yaml
 kubectl apply -f 03-example-crd.yaml
+{ set +x; } 2> /dev/null
 
-# apply/delete 04-example-obj.yaml once kopf is running
-#kubectl apply -f 04-example-obj.yaml
+"${DNAME}/bin/kopf" run example.py --verbose &
+
+KOPF_PID="$!"
+
+for i in `seq 1 10`
+do
+  CMD="apply"
+  [ $(( i % 2 )) = 0 ] && CMD="delete"
+  sleep 2
+  echo "--------------------------------------------------------------------------------"
+  set -x
+  kubectl $CMD -f 04-example-obj.yaml
+  { set +x; } 2> /dev/null
+done
+
+echo "--------------------------------------------------------------------------------"
+set -x
+kill -15 $KOPF_PID
+{ set +x; } 2> /dev/null
+sleep 1
+
