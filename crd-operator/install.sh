0299dcebea29c6b8f47f950d4c927ce35abe3df0
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-12-12 16:26:20 +0000

    crd-operator: 'install.sh' script created to install 'kopf'

---
+++ crd-operator/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Ref: https://kopf.readthedocs.io/en/stable/install/
+
+DNAME="vpy"
+
+set -e -o pipefail
+
+cd "$(dirname "$0")"
+
+set -x
+[ -d "$DNAME" ] || python3 -m virtualenv "$DNAME"
+
+"${DNAME}/bin/pip3" install kopf
+
+#kubectl apply -f https://github.com/nolar/kopf/raw/main/peering.yaml
