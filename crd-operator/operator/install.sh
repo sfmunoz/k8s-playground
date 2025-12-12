8b76db578f8ab1b539c135834ce59bca9c3c5a8f
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-12-12 17:23:28 +0000

    crd-operator: 'install.sh' uses local files

--- crd-operator/operator/install.sh
+++ crd-operator/operator/install.sh
@@ -13,5 +13,10 @@ set -x
 
 "${DNAME}/bin/pip3" install kopf
 
-kubectl apply -f https://github.com/nolar/kopf/raw/main/peering.yaml
-kubectl apply -f https://github.com/nolar/kopf/raw/main/examples/crd.yaml
+# peering split into CRD and OBJ to prevent errors
+kubectl apply -f 01-peering-crd.yaml
+kubectl apply -f 02-peering-obj.yaml
+kubectl apply -f 03-example-crd.yaml
+
+# apply/delete 04-example-obj.yaml once kopf is running
+#kubectl apply -f 04-example-obj.yaml
