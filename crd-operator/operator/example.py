21b7d459f4e242236d635ab241ae80dd9f3a7cb1
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-12-12 18:14:47 +0000

    crd-operator: 'example.py' set to use 'logging'

--- crd-operator/operator/example.py
+++ crd-operator/operator/example.py
@@ -1,7 +1,7 @@
 import kopf
-
+import logging
 
 @kopf.on.create('kopfexamples')
 def create_fn(spec, **kwargs):
-    print(f"And here we are! Creating: {spec}")
+    logging.info(f"create_fn(): spec={spec}")
     return {'message': 'hello world'}  # will be the new status
