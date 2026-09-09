77e3a219d0da856d4b44ec773becc154309da621
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-09-09 15:58:17 +0000

    talos-docker: 'talosctl cluster create' replaced by 'talosctl cluster create docker' and '--controlplanes' dropped

--- talos-docker/install.sh
+++ talos-docker/install.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-[[ "${CONTROLPLANES}" = "" ]] && CONTROLPLANES=2
 [[ "${WORKERS}" = "" ]] && WORKERS=2
 
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | /usr/bin/env NONINTERACTIVE=1 /bin/bash
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew install siderolabs/tap/talosctl kubernetes-cli
 
-talosctl cluster create \
-  --controlplanes $CONTROLPLANES \
+talosctl cluster create docker \
   --workers $WORKERS
 
 cat > /home/vagrant/.bashrc << __EOF
