e2bd0139500e9064aa2038d527665eeeb2f82c7b
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-09-09 16:24:31 +0000

    talos-docker: 'brew tap siderolabs/tap' and 'brew trust siderolabs/tap' used

--- talos-docker/install.sh
+++ talos-docker/install.sh
@@ -4,6 +4,8 @@
 
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | /usr/bin/env NONINTERACTIVE=1 /bin/bash
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+brew tap siderolabs/tap
+brew trust siderolabs/tap
 brew install siderolabs/tap/talosctl kubernetes-cli
 
 talosctl cluster create docker \
